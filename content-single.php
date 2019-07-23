41bae5278e31ceb39f0d8a0d0661dd07acf28a09
Roby <roberta.btt@gmail.com>
2019-07-23 15:55:56 +0200

    Pulizia al single

--- content-single.php
+++ content-single.php
@@ -38,50 +38,9 @@
 
 <div class='post-content'>
     <?php the_content(); ?>
+    <footer>
+        <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'venera' ), 'after' => '</div>' ) ); ?>
+    </footer>
 </div>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="entry-content">
-		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'venera' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-meta">
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'venera' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'venera' ) );
-
-			if ( ! venera_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'venera' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'venera' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'venera' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'venera' );
-				}
-
-			} // end check for categories on this blog
-                        ?> 
-                    
-			<?php printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
-				get_permalink(),
-				the_title_attribute( 'echo=0' )
-			);
-		?>
-
-		<?php edit_post_link( __( 'Edit', 'venera' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-meta -->
-</article><!-- #post-## -->
